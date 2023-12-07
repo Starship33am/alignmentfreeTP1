a95a68989b97ef4cb9efacd86b72d0202c220689
Bren_03 <91853452+Starship33am@users.noreply.github.com>
2023-12-07 01:44:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,33 +3,35 @@ from kmers import enum_kmers, kmer2str
 import time
 
 def similarity(A, inter, B):
-    # --- To complete ---
     return inter/A , inter/B
 
-def jaccard(seqA, seqB, k):
+def jaccard(completeA, completeB, k):
   # Gets seq B and dico_A, outputs taille_I and taille_U
-  seqA=seqA[0]#[:30]
-  seqB=seqB[0]#[:30]
+  
+  #seqA=seqA[0]#[:30]
+  #seqB=seqB[0]#[:30]
   dico={}
   taille_U=0
   taille_I=0
-  for kmer in enum_kmers(seqA,k):
-    taille_U+=1
-    if kmer not in dico:
-      dico[kmer]=1
-    else:
-      dico[kmer]+=1
 
-  for kmer in enum_kmers(seqB,k):
-    if kmer in dico:
-      taille_I+=1
-      dico[kmer]-=1
-      if dico[kmer]==0:
-        del dico[kmer]
-    else:
+  for seqA in completeA:
+    for kmer in enum_kmers(seqA,k):
       taille_U+=1
-  return taille_I/taille_U, taille_I, taille_U
+      if kmer not in dico:
+        dico[kmer]=1
+      else:
+        dico[kmer]+=1
 
+  for seqB in completeB:
+    for kmer in enum_kmers(seqB,k):
+      if kmer in dico:
+        taille_I+=1
+        dico[kmer]-=1
+        if dico[kmer]==0:
+          del dico[kmer]
+      else:
+        taille_U+=1
+  return taille_I/taille_U, taille_I, taille_U
 
 # La plus grande ressemblance entre 2 séquences est de 0.9377
 # La plus petite ressemblance entre 2 séquences est de 0.001768
