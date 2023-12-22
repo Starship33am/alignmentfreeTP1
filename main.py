2e3631967ea3d4848da8e2d31399ae71cde54b79
Bren_03 <91853452+Starship33am@users.noreply.github.com>
2023-12-22 10:27:51 +0100

    Update main.py
    Add second method to reduce time while comparing sequences

--- main.py
+++ main.py
@@ -36,6 +36,58 @@ def jaccard(completeA, completeB, k):
 # La plus grande ressemblance entre 2 séquences est de 0.9377
 # La plus petite ressemblance entre 2 séquences est de 0.001768
 
+# Second method
+def solution_heapq(file,k,s):
+  # Creation des valeurs infini
+  sketch = [float('-inf')]*s
+  # Creation de lobjet heapq avec les valeurs infini
+  heapq.heapify(sketch)
+
+  # Pour tout les kmer, on garde les plus petites valeurs
+  for seq in file:
+    for kmer in enum_kmers(seq,k):
+      # On cherche lelement le plus petit
+      elem=sketch[0]
+      # print('min',sketch[0])
+      # print('complet', sketch)
+      if -kmer>elem:
+        # enlever min
+        heapq.heappop(sketch)
+        # ajout du nouvelle element
+        heapq.heappush(sketch,-kmer)
+        # print(kmer)
+        # print(sketch)
+
+  return sketch
+
+def jaccard2(completeA, completeB):
+  U=0
+  I=0
+  i=0
+  j=0
+
+  tailleA=len(completeA)
+  tailleB=len(completeB)
+  while i<tailleA and j<tailleB:
+    if completeA[i]==completeB[j]:
+      I+=1
+      U+=1
+      i+=1
+      j+=1
+
+    elif completeA[i]>completeB[j]:
+      j+=1
+      U+=1
+
+    else:
+      i+=1
+      U+=1
+
+  U+=(i-tailleA)+(j-tailleB)
+
+  return (I/U), I, U
+
+
 if __name__ == "__main__":
     # Load all the files in a dictionary
     data="/Users/brenda/Mi unidad (brenda.er.0203@gmail.com)/Sorbonne/Phyg/tme5_bren/data/"
@@ -53,6 +105,27 @@ if __name__ == "__main__":
             distance_j, taille_I, taille_U = jaccard(files[filenames[i]], files[filenames[j]], k)
             sima,simb=similarity(len(files[filenames[i]][0]), taille_I, len(files[filenames[j]][0]))
             distances.append([i,j,distance_j,sima,simb])
-
             #print(filenames[i], filenames[j], j)
     print("--- %s seconds ---" % (time.time() - start_time))
+    
+    # Second method
+    k = 21
+    s = 100
+    distances=[]
+    liste_sketch = []
+    start_time = time.time()
+    # Creation de tous les sketchs
+    for i in range(len(files)):
+        print(filenames[i])
+        sketch = solution_heapq(files[filenames[i]],k,s)
+        sketch=np.sort(-np.array(sketch))
+        liste_sketch.append(sketch)
+
+    for i in range (len(files)):
+        for j in range (i+1, len(files)):
+            # --- Complete here ---
+            distance_j, taille_I, taille_U = jaccard2(liste_sketch[i], liste_sketch[j])
+            distances.append([i,j,distance_j])
+
+    end_time = time.time()
+    print("Time : ", end_time-start_time)
